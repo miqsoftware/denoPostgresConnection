8cc4dceb3760484cc3a81015e1082a93d0053db5
Miqueias Rodrigues da Costa <miqsoftware1@hotmail.com>
2021-01-04 21:21:54 -0300

    configurado servidor graphql

---
+++ src/api-graphql/server.js
@@ -0,0 +1,31 @@
+import { Application, Router} from "./deps.js";
+import { applyGraphQL, gql, GQLError } from './deps.js';
+import { PORT } from './config.ts';
+const app = new Application();
+
+const typeDefs = gql`
+    type Mercadoria{
+        id: ID
+    }
+    
+    type Query{
+        mercadorias: [Mercadoria]
+    }
+`;
+
+const resolvers = {
+    Query:{
+        
+    }
+};
+
+const GraphQLService = await applyGraphQL({
+    path: '/graphql',
+    typeDefs,
+    resolvers
+})
+
+app.use(GraphQLService.routes(),
+        GraphQLService.allowedMethods());
+console.log('Server running int port ' + PORT);
+await app.listen({ port: PORT })
